8e37e05f0a19073e15353c612aa76f9531ce611c
Bruno Kemel <br.kemel@gmail.com>
2025-03-25 20:40:28 -0300

    listagem project primeiro commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+
+function script() {
+    return gulp.src('./src/script/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compresset'}))
+    .pipe(gulp.dest('./dist/js'))
+}
+
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+}
+
+exports.default = gulp.parallel(styles, images, script);
+
+exports.watch = function() {
+    gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+    gulp.watch("./src/scripts/*.js", gulp.parallel(script));
+}
